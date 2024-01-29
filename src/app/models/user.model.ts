83d691ef4a84b966de035307bb7d8700e2c99d25
Jaheca1964 <Jaheca1964@users.noreply.github.com>
2024-01-28 19:20:38 -0500

    Primer commit: Descripción de los cambios

---
+++ src/app/models/user.model.ts
@@ -0,0 +1,6 @@
+export interface User{
+    iud: string,
+    email: string,
+    password: string,
+    name: string
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+
+  firebaseConfig: {
+    apiKey: "AIzaSyC5vr1C_0Sx2G34p3Uo1TZYk75fwMBxiUw",
+    authDomain: "product-admin-app-4c3e3.firebaseapp.com",
+    projectId: "product-admin-app-4c3e3",
+    storageBucket: "product-admin-app-4c3e3.appspot.com",
+    messagingSenderId: "727149073092",
+    appId: "1:727149073092:web:d3cdf7ac9d2f84ee8bef91"
+}
+};
